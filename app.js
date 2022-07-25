ec22ef69683eac26e186d8c436addb2dfecbefe9
ErdoganSenturk <erdsen57@gmail.com>
2022-07-26 00:33:48 +0300

    v1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const submitBtn = document.getElementById('submit-btn')
 submitBtn.addEventListener("click", function(){
-    const inputbtn = document.getElementById('input-btn').value;
+    const inputbtn = document.getElementById('input-btn');
  
 
 const key = "ceb84ee212e3c4957be78d213c7ac491";
@@ -9,18 +9,14 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}
       .then((response) => response.json())
       .then((data) => {
         const cityName = data.name;
-        const tem = data.main.temp;
-        const temp = tem - 273.15;
+        const temp = data.main.temp;
         const description = data.weather[0].description;
         document.getElementById('city').innerText = cityName;
         document.getElementById('temp').innerText = temp.toFixed();
         document.getElementById('des').innerText = description;
+        console.log(cityName);
       })
-      .catch((res) => alert("Please Enter a Right City Name"));
+      .catch((res) => console.log(error));
 }
 )
 
-// const key = "ceb84ee212e3c4957be78d213c7ac491";
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&APPID=${key}`;
-
-// `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}&units=metric&appid=bdd3a9eeeb496f366d2a0051b35ac744`;
