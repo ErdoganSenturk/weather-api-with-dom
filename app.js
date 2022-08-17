ca8c10a253f6247d6930de5d2aa25206e20cca75
ErdoganSenturk <erdsen57@gmail.com>
2022-08-18 00:18:05 +0300

    v2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const submitBtn = document.getElementById('submit-btn')
+const clear = document.getElementById("clear");
+
 submitBtn.addEventListener("click", function(){
     const inputbtn = document.getElementById('input-btn');
  
@@ -10,13 +12,16 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}
       .then((data) => {
         const cityName = data.name;
         const temp = data.main.temp;
+    
         const description = data.weather[0].description;
         document.getElementById('city').innerText = cityName;
         document.getElementById('temp').innerText = temp.toFixed();
-        document.getElementById('des').innerText = description;
-        console.log(cityName);
+        document.getElementById("des").innerText = description;
+       
+        
       })
       .catch((res) => console.log(error));
 }
 )
+
 
