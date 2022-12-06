bd4c385e017bce1cbf5b2a09c236819b5ffabc44
ErdoganSenturk <erdsen57@gmail.com>
2022-12-06 22:45:07 +0300

    lang

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inputbtn = document.getElementById('input-btn');
 
 submitBtn.addEventListener("click", function(){
 const key = "ceb84ee212e3c4957be78d213c7ac491";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}&units=metric&APPID=${key}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}&units=metric&APPID=${key}&lang=tr`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
