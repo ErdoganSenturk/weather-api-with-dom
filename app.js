45ef16a35113f7bac3f3703162250e50fbd76081
ErdoganSenturk <erdsen57@gmail.com>
2022-12-06 20:17:41 +0300

    minor rev

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const submitBtn = document.getElementById('submit-btn')
 const clear = document.getElementById("clear");
+const inputbtn = document.getElementById('input-btn');
+
 
 submitBtn.addEventListener("click", function(){
-    const inputbtn = document.getElementById('input-btn');
- 
-
 const key = "ceb84ee212e3c4957be78d213c7ac491";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputbtn.value}&units=metric&APPID=${key}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        const cityName = data.name;
-        const temp = data.main.temp;
-    
-        const description = data.weather[0].description;
-        document.getElementById('city').innerText = cityName;
-        document.getElementById('temp').innerText = temp.toFixed();
+        const { name, main:{ temp }, weather } = data
+        const description = weather[0].description;
+        document.getElementById('city').innerText = name;
+        document.getElementById('temp').innerText = temp.toFixed(1);
         document.getElementById("des").innerText = description;
-       
-        
       })
-      .catch((res) => console.log(error));
+    .catch((error) => console.log(error));
 }
 )
+clear.addEventListener("click", () => { 
+  inputbtn.value = " "
+})
 
 
+window.onload = function () {
+  inputbtn.focus();
+};
